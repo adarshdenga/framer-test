c549f03e9dacae489054a6c1742022ca79adf020
mynameadarsh <adarshdenga@gmail.com>
2024-03-13 20:38:52 +0530

    more animations

--- src/components/First.tsx
+++ src/components/First.tsx
@@ -44,7 +44,7 @@ export default function First() {
           className="w-20 h-20 bg-stone-100 rounded-full"
           initial={{ opacity: 0, y: -100 }}
           animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1, ease: "easeOut", delay: 0.2 }}
+          transition={{ duration: 1, ease: "easeOut", delay: 0.4 }}
         ></motion.div>
       </motion.div>
 
@@ -52,12 +52,30 @@ export default function First() {
       <motion.div
         variants={itemVariants}
         className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"
-      ></motion.div>
+      >
+        <motion.div
+          className="w-1/3 h-1/3 shadow-md bg-rose-400"
+          animate={{
+            scale: [1, 2, 2, 1],
+            rotate: [0, 90, 90, 0],
+            borderRadius: ["10%", "10%", "50%", "10%"],
+          }}
+          transition={{ duration: 5, ease: "easeInOut", repeat: Infinity }}
+        ></motion.div>
+      </motion.div>
 
+      {/* Button */}
       <motion.div
         variants={itemVariants}
         className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"
-      ></motion.div>
+      >
+        <motion.button
+          whileTap={{ scale: 0.9 }}
+          className="bg-emerald-500 w-1/2 py-4 rounded-lg text-2xl text-gray-100 font-light tracking-wide"
+        >
+          Click me!
+        </motion.button>
+      </motion.div>
 
       <motion.div
         variants={itemVariants}
