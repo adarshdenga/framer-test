3dafceb1f171c422a356ad95a8978457fb2598fe
mynameadarsh <adarshdenga@gmail.com>
2024-04-01 00:03:14 +0200

    Changed animations from framer-motion to CSS keyframes for smoothness

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,209 +1,155 @@
-import { motion } from "framer-motion";
+import pica from "../assets/stockimages/pica.png";
+import picb from "../assets/stockimages/picb.png";
+import picc from "../assets/stockimages/picc.png";
+import picd from "../assets/stockimages/picd.png";
+import pice from "../assets/stockimages/pice.png";
+import picf from "../assets/stockimages/picf.png";
+import picg from "../assets/stockimages/picg.png";
+import pich from "../assets/stockimages/pich.png";
+import pici from "../assets/stockimages/pici.png";
+import picj from "../assets/stockimages/picj.png";
+import pick from "../assets/stockimages/pick.png";
+import picl from "../assets/stockimages/picl.png";
+import picm from "../assets/stockimages/picm.png";
+import picn from "../assets/stockimages/picn.png";
+import pico from "../assets/stockimages/pico.png";
+import picp from "../assets/stockimages/picp.png";
+import picq from "../assets/stockimages/picq.png";
+import picr from "../assets/stockimages/picr.png";
+import pics from "../assets/stockimages/pics.png";
+import pict from "../assets/stockimages/pict.png";
+import picu from "../assets/stockimages/picu.png";
+import picv from "../assets/stockimages/picv.png";
+import picw from "../assets/stockimages/picw.png";
+import picx from "../assets/stockimages/picx.png";
+import picy from "../assets/stockimages/picy.png";
+import { useState, useEffect } from "react";
 
 export default function Home() {
-  const columnVariants = {
-    hidden: {
-      opacity: 0,
-    },
-    visible: {
-      opacity: 1,
-      transition: {
-        staggerChildren: 0.25,
-      },
-    },
-  };
+  const [animationStarted, setAnimationStarted] = useState(false);
 
-  const imageUpVariants = {
-    hidden: {
-      y: 1500,
-    },
-    visible: {
-      y: 0,
-      transition: { duration: 3, ease: [0, 0.52, 0.92, 0.31] },
-    },
-  };
-
-  const imageDownVariants = {
-    hidden: {
-      y: -1500,
-    },
-    visible: {
-      y: 0,
-      transition: { duration: 3, ease: [0, 0.52, 0.92, 0.31] },
-    },
-  };
+  useEffect(() => {
+    // Trigger animation when component mounts
+    setAnimationStarted(true);
+  }, []);
 
   return (
-    <div className="flex flex-row gap-10">
-      {/* B - T  */}
-      <motion.div
-        className="flex flex-col-reverse w-1/2 gap-10"
-        variants={columnVariants}
-        initial="hidden"
-        animate="visible"
-      >
-        <motion.div
-          variants={imageDownVariants}
-          className="bg-stone-950 aspect-video rounded-lg"
-        ></motion.div>
-
-        <motion.div
-          variants={imageDownVariants}
-          className="bg-stone-950 aspect-video rounded-lg"
-          transition={{ delay: 3 }}
-        ></motion.div>
-
-        <motion.div
-          variants={imageDownVariants}
-          className="bg-stone-950 aspect-video rounded-lg"
-        ></motion.div>
-
-        <motion.div
-          variants={imageDownVariants}
-          className="bg-stone-950 aspect-video rounded-lg"
-        ></motion.div>
-
-        <motion.div
-          variants={imageDownVariants}
-          className="bg-stone-950 aspect-video rounded-lg"
-        ></motion.div>
-      </motion.div>
-
-      {/* T - B  */}
-      <motion.div
-        className="flex flex-col w-1/2 gap-10 overflow-y-hidden "
-        variants={columnVariants}
-        initial="hidden"
-        animate="visible"
-      >
-        <motion.div
-          variants={imageUpVariants}
-          className="bg-stone-950 aspect-video rounded-lg"
-        ></motion.div>
-
-        <motion.div
-          variants={imageUpVariants}
-          className="bg-stone-950 aspect-video rounded-lg"
-          transition={{ delay: 3 }}
-        ></motion.div>
-
-        <motion.div
-          variants={imageUpVariants}
-          className="bg-stone-950 aspect-video rounded-lg"
-        ></motion.div>
-
-        <motion.div
-          variants={imageUpVariants}
-          className="bg-stone-950 aspect-video rounded-lg"
-        ></motion.div>
-
-        <motion.div
-          variants={imageUpVariants}
-          className="bg-stone-950 aspect-video rounded-lg"
-        ></motion.div>
-      </motion.div>
-
-      {/* B - T  */}
-      <motion.div
-        className="flex flex-col-reverse w-1/2 gap-10"
-        variants={columnVariants}
-        initial="hidden"
-        animate="visible"
-      >
-        <motion.div
-          variants={imageDownVariants}
-          className="bg-stone-950 aspect-video rounded-lg"
-        ></motion.div>
-
-        <motion.div
-          variants={imageDownVariants}
-          className="bg-stone-950 aspect-video rounded-lg"
-          transition={{ delay: 3 }}
-        ></motion.div>
-
-        <motion.div
-          variants={imageDownVariants}
-          className="bg-stone-950 aspect-video rounded-lg"
-        ></motion.div>
-
-        <motion.div
-          variants={imageDownVariants}
-          className="bg-stone-950 aspect-video rounded-lg"
-        ></motion.div>
-
-        <motion.div
-          variants={imageDownVariants}
-          className="bg-stone-950 aspect-video rounded-lg"
-        ></motion.div>
-      </motion.div>
-
-      {/* T - B  */}
-      <motion.div
-        className="flex flex-col w-1/2 gap-10 overflow-y-hidden "
-        variants={columnVariants}
-        initial="hidden"
-        animate="visible"
-      >
-        <motion.div
-          variants={imageUpVariants}
-          className="bg-stone-950 aspect-video rounded-lg"
-        ></motion.div>
-
-        <motion.div
-          variants={imageUpVariants}
-          className="bg-stone-950 aspect-video rounded-lg"
-          transition={{ delay: 3 }}
-        ></motion.div>
-
-        <motion.div
-          variants={imageUpVariants}
-          className="bg-stone-950 aspect-video rounded-lg"
-        ></motion.div>
-
-        <motion.div
-          variants={imageUpVariants}
-          className="bg-stone-950 aspect-video rounded-lg"
-        ></motion.div>
-
-        <motion.div
-          variants={imageUpVariants}
-          className="bg-stone-950 aspect-video rounded-lg"
-        ></motion.div>
-      </motion.div>
-
-      {/* B - T  */}
-      <motion.div
-        className="flex flex-col-reverse w-1/2 gap-10"
-        variants={columnVariants}
-        initial="hidden"
-        animate="visible"
-      >
-        <motion.div
-          variants={imageDownVariants}
-          className="bg-stone-950 aspect-video rounded-lg"
-        ></motion.div>
-
-        <motion.div
-          variants={imageDownVariants}
-          className="bg-stone-950 aspect-video rounded-lg"
-          transition={{ delay: 3 }}
-        ></motion.div>
-
-        <motion.div
-          variants={imageDownVariants}
-          className="bg-stone-950 aspect-video rounded-lg"
-        ></motion.div>
-
-        <motion.div
-          variants={imageDownVariants}
-          className="bg-stone-950 aspect-video rounded-lg"
-        ></motion.div>
-
-        <motion.div
-          variants={imageDownVariants}
-          className="bg-stone-950 aspect-video rounded-lg"
-        ></motion.div>
-      </motion.div>
+    <div className="imgboard flex flex-row gap-10">
+      {/* B - T */}
+      <div className="bottomToTopCols flex flex-col-reverse w-1/5 gap-10">
+        <img
+          className={animationStarted ? "aspect-video object-cover" : "hidden"}
+          src={pica}
+        ></img>
+        <img
+          className={animationStarted ? "aspect-video object-cover" : "hidden"}
+          src={picb}
+        ></img>
+        <img
+          className={animationStarted ? "aspect-video object-cover" : "hidden"}
+          src={picc}
+        ></img>
+        <img
+          className={animationStarted ? "aspect-video object-cover" : "hidden"}
+          src={picd}
+        ></img>
+        <img
+          className={animationStarted ? "aspect-video object-cover" : "hidden"}
+          src={pice}
+        ></img>
+      </div>
+      {/* T - B*/}
+      <div className="topToBottomCols flex flex-col w-1/5 gap-10">
+        <img
+          className={animationStarted ? "aspect-video object-cover" : "hidden"}
+          src={picf}
+        ></img>
+        <img
+          className={animationStarted ? "aspect-video object-cover" : "hidden"}
+          src={picg}
+        ></img>
+        <img
+          className={animationStarted ? "aspect-video object-cover" : "hidden"}
+          src={pich}
+        ></img>
+        <img
+          className={animationStarted ? "aspect-video object-cover" : "hidden"}
+          src={pici}
+        ></img>
+        <img
+          className={animationStarted ? "aspect-video object-cover" : "hidden"}
+          src={picj}
+        ></img>
+      </div>
+      {/* B - T */}
+      <div className="bottomToTopCols flex flex-col-reverse w-1/5 gap-10">
+        <img
+          className={animationStarted ? "aspect-video object-cover" : "hidden"}
+          src={pick}
+        ></img>
+        <img
+          className={animationStarted ? "aspect-video object-cover" : "hidden"}
+          src={picl}
+        ></img>
+        <img
+          className={animationStarted ? "aspect-video object-cover" : "hidden"}
+          src={picm}
+        ></img>
+        <img
+          className={animationStarted ? "aspect-video object-cover" : "hidden"}
+          src={picn}
+        ></img>
+        <img
+          className={animationStarted ? "aspect-video object-cover" : "hidden"}
+          src={pico}
+        ></img>
+      </div>
+      {/* T - B*/}
+      <div className="topToBottomCols flex flex-col w-1/5 gap-10">
+        <img
+          className={animationStarted ? "aspect-video object-cover" : "hidden"}
+          src={picp}
+        ></img>
+        <img
+          className={animationStarted ? "aspect-video object-cover" : "hidden"}
+          src={picq}
+        ></img>
+        <img
+          className={animationStarted ? "aspect-video object-cover" : "hidden"}
+          src={picr}
+        ></img>
+        <img
+          className={animationStarted ? "aspect-video object-cover" : "hidden"}
+          src={pics}
+        ></img>
+        <img
+          className={animationStarted ? "aspect-video object-cover" : "hidden"}
+          src={pict}
+        ></img>
+      </div>
+      {/* B - T */}
+      <div className="bottomToTopCols flex flex-col-reverse w-1/5 gap-10">
+        <img
+          className={animationStarted ? "aspect-video object-cover" : "hidden"}
+          src={picu}
+        ></img>
+        <img
+          className={animationStarted ? "aspect-video object-cover" : "hidden"}
+          src={picv}
+        ></img>
+        <img
+          className={animationStarted ? "aspect-video object-cover" : "hidden"}
+          src={picw}
+        ></img>
+        <img
+          className={animationStarted ? "aspect-video object-cover" : "hidden"}
+          src={picx}
+        ></img>
+        <img
+          className={animationStarted ? "aspect-video object-cover" : "hidden"}
+          src={picy}
+        ></img>
+      </div>
     </div>
   );
 }
