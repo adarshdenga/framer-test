fc4522d8ad5642ef5f134535e82ce6d9a8e65466
mynameadarsh <adarshdenga@gmail.com>
2024-04-03 08:08:36 +0200

    Decent exploration into 3D

--- src/components/Phone.tsx
+++ src/components/Phone.tsx
@@ -3,7 +3,10 @@ import Spline from "@splinetool/react-spline";
 export default function Phone() {
   return (
     <div className="flex flex-row w-screen h-screen blue-gradient">
-      <Spline scene="https://prod.spline.design/Qsgv6HRiUh7AzQqi/scene.splinecode"></Spline>
+      <div className="text-white absolute bottom-72 left-48 pointer-events-none">
+        <div className="font-barlow text-6xl font-bold">RED BALLS.</div>
+      </div>
+      <Spline scene="https://prod.spline.design/C2x-eDCr2bTfe5S9/scene.splinecode"></Spline>
     </div>
   );
 }
